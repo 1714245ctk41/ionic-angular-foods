a225c86930146c5a3e168e51a89e47bdbdcfa983
super3636 <58015656+super3636@users.noreply.github.com>
2020-10-20 18:25:05 +0700

    Add files via upload
    add page user

---
+++ src/app/pages/user/user.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CrudProductService } from "../../services/crud-database";
+import { NavController } from "@ionic/angular";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.page.html',
+  styleUrls: ['./user.page.scss'],
+})
+export class UserPage implements OnInit {
+
+  constructor(    private userService:CrudProductService,
+    private nav:NavController,) { }
+
+  ngOnInit() {
+  }
+
+}
